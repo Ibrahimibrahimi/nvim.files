75f4207f25d945393c46cea383daceb8f6928462
lll <lll>
2026-08-13 11:16:26 +0100

    backup successfull

--- automate_backup.sh
+++ automate_backup.sh
@@ -1,10 +1,16 @@
 # TODO put this script in ~/.config/autostart
 
 # make the folder if not found
+cd ~/nvim.files
+
+
 mkdir -p nvim
 
 # copy from ~/.config/nvim
-cp ~/.config/nvim/{.stylua.toml,init.lua,lazy-lock.json,LICENSE,lua} nvim/ -r
+cp ~/.config/nvim/{.stylua.toml,init.lua,lazy-lock.json,LICENSE,lua} ~/nvim.files/nvim/ -r
+
+# TODO log
+echo "[INFO] .files copied" >> ~/nvim.files/history.log
 
 echo "Copied !"
 
@@ -14,3 +20,4 @@ git commit -m "backup successfull"
 
 git push
 
+echo "[INFO] Backup Successfull" >> ~/nvim.files/history.log
