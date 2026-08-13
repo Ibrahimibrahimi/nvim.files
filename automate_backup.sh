874991b4cbdf4700311cc3b6bbc9079a97a8385d
lll <lll>
2026-08-13 11:03:05 +0100

    backup successfull

--- automate_backup.sh
+++ automate_backup.sh
@@ -1,7 +1,7 @@
 # TODO put this script in ~/.config/autostart
 
 # copy from ~/.config/nvim
-cp ~/.config/nvim/{init.lua,lazy-lock.json,LICENSE,lua,README.md} .
+cp ~/.config/nvim/{init.lua,lazy-lock.json,LICENSE,lua,README.md} . -r
 
 echo "Copied !"
 
